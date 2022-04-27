330340b2062698477d94c0b343989ddffae3d0a2
steven.yan <810824494@qq.com>
2022-04-27 17:58:39 +0800

    Add confusion matrix plot function

--- utils.py
+++ utils.py
@@ -32,7 +32,41 @@ class LabelSmoothingCrossEntropy(nn.Module):
                 loss = loss.mean()
         return loss * self.eps / c + (1 - self.eps) * F.nll_loss(log_preds, target, reduction=self.reduction)
 
+def plot_confusion_matrix(y_true, y_pred, d, save_folder, title='Confusion Matrix', cmap=plt.cm.ocean):
+    labels = d.keys()
+    tick_marks = np.array(range(len(labels))) + 0.5
+    cm = confusion_matrix(y_true, y_pred)  
+    np.set_printoptions(precision=2)
 
+    cm_normalized = cm.astype(float) / cm.sum(axis=1)[:, np.newaxis]  
+    plt.figure(figsize=(12, 8), dpi=120)
+
+    ind_array = np.arange(len(labels))
+    x, y = np.meshgrid(ind_array, ind_array)
+
+    for x_val, y_val in zip(x.flatten(), y.flatten()):
+        c = cm_normalized[y_val][x_val]
+        if c > 0.01:
+            plt.text(x_val, y_val, "%0.2f" % (c,), color='red', fontsize=7, va='center', ha='center')
+
+    title = 'Normalized confusion matrix'
+    plt.imshow(cm_normalized, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    xlocations = np.array(range(len(labels)))
+    plt.xticks(xlocations, labels, rotation=45)
+    plt.yticks(xlocations, labels)
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    # offset the tick
+    plt.gca().set_xticks(tick_marks, minor=True)
+    plt.gca().set_yticks(tick_marks, minor=True)
+    plt.gca().xaxis.set_ticks_position('none')
+    plt.gca().yaxis.set_ticks_position('none')
+    plt.grid(True, which='minor', linestyle='-')
+    plt.gcf().subplots_adjust(bottom=0.15)
+    plt.savefig(os.path.join(save_folder, 'confusion_matrix.png'), format='png')
+    #plt.show()
 
 def write_pickle(list_info: list, file_name: str):
     with open(file_name, 'wb') as f:
